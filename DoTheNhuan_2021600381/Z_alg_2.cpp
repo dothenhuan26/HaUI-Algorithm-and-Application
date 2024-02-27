482f57ab16e7aa3ef2bd80377f5947f4ad13dacf
nhuandt26 <dothenhuan26@gmail.com>
2024-02-28 00:50:45 +0700

    up

---
+++ DoTheNhuan_2021600381/Z_alg_2.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+bool Z(string p, string t, int *z) {
+	string s = p+"$"+t;
+	int p_len=p.length(), n=s.length(), left=0, right=0;
+	
+	z[0]=-1;
+	for(int i=1; i<n; i++) z[i]=0;
+	
+	for(int i=1; i<n; i++) {
+		if(i>right) {
+			left=right=i;
+			while(right<n && s[right-left]==s[right]) right++;
+			z[i]=right-left;
+			right--;
+		} else if(z[i-left]<right-i+1) {
+			z[i]=z[right-left];
+		} else {
+			left=i;
+			while(right<n && s[right-left]==s[right]) right++;
+			z[i]=right-left;
+			right--;
+		}
+	}
+	
+	for(int i=0; i<n; i++) {
+		if(z[i]==p_len) return true;
+	}
+	return false;
+}
+
+
+bool Z_alg(string p, string t, int *z) {
+	string s = p+"$"+t;
+	int p_len=p.length(), n=s.length(), left=0, right=0;
+	
+	z[0]=-1;
+	for(int i=1; i<n; i++) z[i]=0;
+	
+	for(int i=1; i<n; i++) {
+		if(i>right) {
+			left=right=i;
+			while(right<n && s[right-left]==s[right]) right++;
+			z[i]=right-left;
+			right--;
+		} else if(z[i-left]<right-i+1) {
+			z[i] = z[right-left];
+		} else {
+			left=i;
+			while(right<n && s[right-left]==s[right]) right++;
+			z[i]=right-left;
+			right--;
+		}
+	}
+	
+	for(int i=0; i<n; i++) if(p_len==z[i]) return true;
+	return false;
+}
+
+int main() {
+	string p = "con";
+	string t = "chuoi con nam trong chuoi to la mot chuoi con";
+	int z[1001];
+	if(Z(p, t, z)) {
+		cout<<"Co";
+	} else { 
+		cout<<"Khong";
+	}
+
+	return 0;
+}
+
